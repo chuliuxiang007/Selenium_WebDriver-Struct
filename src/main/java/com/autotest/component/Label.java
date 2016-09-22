51f11d1591e17ff7bdc3cc2462bf792cc3194fa3
chuliuxiang007 <liushuaiworld@163.com>
2016-09-22 19:05:36 +0800

    ADD MAINPAGE

---
+++ src/main/java/com/autotest/component/Label.java
@@ -0,0 +1,51 @@
+package com.autotest.component;
+
+import org.openqa.selenium.WebDriver;
+
+import com.autotest.Control;
+import com.autotest.ExtJSPage;
+import com.autotest.utils.Locator;
+
+public class Label extends Control {
+	
+	private String path;
+	private String id;
+
+	public Label(WebDriver webDriver,String path) {
+		super(webDriver);
+		this.path =path;
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getQuery(){
+		return "Ext.ComponentQuery.query"+path;
+	}
+	
+	/**
+	 * 获取label的ID号
+	 */
+	public void getID() {
+		this.path=this.getQuery();
+		String js="return " + this.path + ".id";
+		this.id=getId(js);
+		System.out.println("the id is "+id);
+	}
+	
+	/**
+	 * 校验label标签是否存在
+	 */
+	public boolean existLabel(){
+		boolean flag=false;
+		getID();
+		Locator locator = new Locator(this.id);
+		ExtJSPage extPage=new ExtJSPage(webDriver);
+		try {
+			flag=extPage.isElementPresent(locator, 60);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return flag;
+	}
+}
---
+++ src/main/java/com/autotest/pageconstant/LoginPageConstant.java
@@ -0,0 +1,10 @@
+package com.autotest.pageconstant;
+
+public class LoginPageConstant {
+
+	//Login页面元素对象位置
+	public static String userName="(\"textfield[name='username']\")[0]";
+	public static String password="(\"textfield[name='password']\")[0]";
+	public static String  btnPath="(\"button[text='登录']\")[0].el";
+	
+}
